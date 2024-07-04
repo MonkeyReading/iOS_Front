4325ca0d057a6416b7fe4d0db54842a6494a25c6
JEONG-J <ikejhc159@gmail.com>
2024-07-04 21:05:34 +0900

    카카오 로그인 매니저 구현

--- MonkeyRading/MonkeyRading/Kakao/KakaoLoginManager.swift
+++ MonkeyRading/MonkeyRading/Kakao/KakaoLoginManager.swift
@@ -1,33 +1,33 @@
-////
-////  KakaoLoginManager.swift
-////  MonkeyRading
-////
-////  Created by 정의찬 on 7/4/24.
-////
 //
-//import Foundation
-//import KakaoSDKAuth
-//import KakaoSDKUser
+//  KakaoLoginManager.swift
+//  MonkeyRading
 //
-//class KakaoLoginManager: ObservableObject {
-//    fileprivate func handleLoginResponse(oauthToken: OAuthToken?, error: Error?, completion: @escaping (Result<OAuthToken, Error>) -> Void) {
-//        if let error = error {
-//            completion(.failure(error))
-//        } else if let oauthToken = oauthToken {
-//            completion(.success(oauthToken))
-//        }
-//    }
-//    
-//    internal func kakaoLogin(completion: @escaping (Result<OAuthToken, Error>) -> Void) {
-//        if UserApi.isKakaoTalkLoginAvailable() {
-//            UserApi.shared.loginWithKakaoTalk { [weak self] (oathToken, error) in
-//                self?.handleLoginResponse(oauthToken: oathToken, error: error, completion: completion)
-//            }
-//        } else {
-//            UserApi.shared.loginWithKakaoAccount { [weak self] (oauthToken, error) in
-//                self?.handleLoginResponse(oauthToken: oauthToken, error: error, completion: completion)
-//            }
-//        }
-//    }
-//}
+//  Created by 정의찬 on 7/4/24.
 //
+
+import Foundation
+import KakaoSDKAuth
+import KakaoSDKUser
+
+class KakaoLoginManager: ObservableObject {
+    fileprivate func handleLoginResponse(oauthToken: OAuthToken?, error: Error?, completion: @escaping (Result<OAuthToken, Error>) -> Void) {
+        if let error = error {
+            completion(.failure(error))
+        } else if let oauthToken = oauthToken {
+            completion(.success(oauthToken))
+        }
+    }
+    
+    internal func kakaoLogin(completion: @escaping (Result<OAuthToken, Error>) -> Void) {
+        if UserApi.isKakaoTalkLoginAvailable() {
+            UserApi.shared.loginWithKakaoTalk { [weak self] (oathToken, error) in
+                self?.handleLoginResponse(oauthToken: oathToken, error: error, completion: completion)
+            }
+        } else {
+            UserApi.shared.loginWithKakaoAccount { [weak self] (oauthToken, error) in
+                self?.handleLoginResponse(oauthToken: oauthToken, error: error, completion: completion)
+            }
+        }
+    }
+}
+
