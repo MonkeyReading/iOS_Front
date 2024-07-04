299e7240254a1b2137d1c8a6d94164cc731e78b4
JEONG-J <ikejhc159@gmail.com>
2024-07-04 16:29:33 +0900

    📝 깃 이그노어
    깃 이그노어 파일 생성

---
+++ BookAR/BookAR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookAR
+//
+//  Created by 정의찬 on 7/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
