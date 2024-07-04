aa3989aa402d878e0a4a022512db518e6a7fbd1a
JEONG-J <ikejhc159@gmail.com>
2024-07-04 21:34:39 +0900

    🎨 로그인 뷰모델 작성
    로그인 뷰모델 작성

--- MonkeyRading/MonkeyRading/Kakao/LoginManager.swift
+++ MonkeyRading/MonkeyRading/Kakao/LoginManager.swift
@@ -1,16 +1,17 @@
 //
-//  KakaoLoginManager.swift
+//  LoginManager.swift
 //  MonkeyRading
 //
 //  Created by 정의찬 on 7/4/24.
 //
-
 import Foundation
 import KakaoSDKAuth
 import KakaoSDKUser
 
-class KakaoLoginManager: ObservableObject {
-    fileprivate func handleLoginResponse(oauthToken: OAuthToken?, error: Error?, completion: @escaping (Result<OAuthToken, Error>) -> Void) {
+
+class LoginManager: ObservableObject  {
+    
+    func handleLoginResponse(oauthToken: OAuthToken?, error: Error?, completion: @escaping (Result<OAuthToken, Error>) -> Void) {
         if let error = error {
             completion(.failure(error))
         } else if let oauthToken = oauthToken {
@@ -18,7 +19,7 @@ class KakaoLoginManager: ObservableObject {
         }
     }
     
-    internal func kakaoLogin(completion: @escaping (Result<OAuthToken, Error>) -> Void) {
+   func kakaoLogin(completion: @escaping (Result<OAuthToken, Error>) -> Void) {
         if UserApi.isKakaoTalkLoginAvailable() {
             UserApi.shared.loginWithKakaoTalk { [weak self] (oathToken, error) in
                 self?.handleLoginResponse(oauthToken: oathToken, error: error, completion: completion)
@@ -30,4 +31,3 @@ class KakaoLoginManager: ObservableObject {
         }
     }
 }
-
