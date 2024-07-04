227660198efe4ec1c06e82f84bd8e7300a7efe24
JEONG-J <ikejhc159@gmail.com>
2024-07-04 20:26:49 +0900

    🎨 카카오 로그인 구현
    카카오 로그인 구현

--- MonkeyRading/MonkeyRading/Kakao/KakaoLoginManager.swift
+++ MonkeyRading/MonkeyRading/Kakao/KakaoLoginManager.swift
@@ -9,3 +9,25 @@ import Foundation
 import KakaoSDKAuth
 import KakaoSDKUser
 
+class KakaoLoginManager: ObservableObject {
+    fileprivate func handleLoginResponse(oauthToken: OAuthToken?, error: Error?, completion: @escaping (Result<OAuthToken, Error>) -> Void) {
+        if let error = error {
+            completion(.failure(error))
+        } else if let oauthToken = oauthToken {
+            completion(.success(oauthToken))
+        }
+    }
+    
+    internal func kakaoLogin(completion: @escaping (Result<OAuthToken, Error>) -> Void) {
+        if UserApi.isKakaoTalkLoginAvailable() {
+            UserApi.shared.loginWithKakaoTalk { [weak self] (oathToken, error) in
+                self?.handleLoginResponse(oauthToken: oathToken, error: error, completion: completion)
+            }
+        } else {
+            UserApi.shared.loginWithKakaoAccount { [weak self] (oauthToken, error) in
+                self?.handleLoginResponse(oauthToken: oauthToken, error: error, completion: completion)
+            }
+        }
+    }
+}
+
